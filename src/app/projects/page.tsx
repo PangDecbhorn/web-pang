d92d6e468cd20395609ecf9bf988530a92a8d230
Jittrabhorn <jittrabhorn@sleekev.com>
2025-08-18 16:35:40 +0700

    set z-0 at /project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -73,7 +73,7 @@ function ProjectPage() {
         </div> */}
 
         {/* Grid Cards */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 z-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 z-0">
           {[...projects]
             .sort(
               (a, b) =>
